9526b093200a2b2054b2cec728cff12807ae274b
kamil wroniewicz <qu4ku@hotmail.com>
2017-07-06 15:20:21 +0200

    templates

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse('Hello World')
+    todos = Todo.objects.all()[:10]
+    context = {
+        'name': 'Brad',
+        'todos': todos,
+    }
+    return render(request, 'index.html', context)
+
+
+def details(request, id):
+    todo = Todo.objects.get(id=id)
+
+    context = {
+        'todo': todo
+    }
+
+    return render(request, 'details.html', context)
